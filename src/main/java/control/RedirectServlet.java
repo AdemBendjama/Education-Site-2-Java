daf7e6fc9f23f338679d28e3c7ada9f0b4336a5b
adem bendjama <93732841+AdemBendjama@users.noreply.github.com>
2022-12-31 16:46:17 +0100

    Redirect pages based on URL and user session

---
+++ src/main/java/control/RedirectServlet.java
@@ -0,0 +1,45 @@
+package control;
+
+import model.UserManager;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.Serial;
+
+@WebServlet(name = "RedirectServlet", value = "/redirect")
+public class RedirectServlet extends HttpServlet {
+    //
+    @Serial
+    private static final long serialVersionUID = 102831973239L;
+
+    public RedirectServlet() {
+        super();
+    }
+    //
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //
+        RequestDispatcher dispatcher;
+        HttpSession session = request.getSession();
+        UserManager userManager = new UserManager();
+
+        if (session.getAttribute("admin") != null) {
+            //
+            UserListServlet userListServlet = new UserListServlet();
+            userListServlet.doGet(request,response);
+
+        } else if (session.getAttribute("teacher") != null) {
+            //
+            UserListServlet userListServlet = new UserListServlet();
+            userListServlet.doGet(request,response);
+
+        } else if (session.getAttribute("student") != null) {
+            //
+            dispatcher = request.getRequestDispatcher("/WEB-INF/Student/StudentMain.jsp");
+            dispatcher.include(request, response);
+
+        }
+    }
+}
