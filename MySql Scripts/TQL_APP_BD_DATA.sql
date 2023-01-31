accd1a05ae556baa81362791478debfa02c67523
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-01-31 10:05:20 +0100

    Add Accounts

--- MySql Scripts/TQL_APP_BD_DATA.sql
+++ MySql Scripts/TQL_APP_BD_DATA.sql
@@ -5,6 +5,10 @@ INSERT INTO `TQL_APP_BD`.`admins` (`admin_email`, `admin_name`, `admin_password`
 INSERT INTO `TQL_APP_BD`.`users` (`user_email`, `user_name`, `user_password`,`user_rank`) VALUES ('adambendjamaa2013@gmail.com', 'Adam Bendjamaa', 'adambendjamaa','student');
 INSERT INTO `TQL_APP_BD`.`users` (`user_email`, `user_name`, `user_password`,`user_rank`) VALUES ('ronswanson@gmail.com', 'Ron Swanson', 'ronswanson','teacher');
 
+INSERT INTO `TQL_APP_BD`.`admins` (`admin_email`, `admin_name`, `admin_password`) VALUES ('admin@gmail.com', 'admin', 'admin');
+INSERT INTO `TQL_APP_BD`.`users` (`user_email`, `user_name`, `user_password`,`user_rank`) VALUES ('teacher@gmail.com', 'teacher', 'teacher','teacher');
+INSERT INTO `TQL_APP_BD`.`users` (`user_email`, `user_name`, `user_password`,`user_rank`) VALUES ('student@gmail.com', 'student', 'student','student');
+
 
 
 
--- README.md
+++ README.md
@@ -4,3 +4,16 @@ E-Learning Site that i created before , but now i added to it a functional backe
 
 ## Github Repository
 https://github.com/AdemBendjama/Education-Site-2-Java
+
+## Accounts
+### Admin
+> Email : admin@gmail.com
+> Password : admin
+### Teacher
+> Email : student@gmail.com
+> Password : teacher
+### Student
+> Email : student@gmail.com
+> Password : student
+
+# ScreenShots
